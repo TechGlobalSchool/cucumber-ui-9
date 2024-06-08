403d6c42fb705d766b9dfc782eedbde1f3af8bc3
TechGlobalSchool <tgschool01@gmail.com>
2024-06-08 14:00:43 -0500

    Data Table

---
+++ cypress/e2e/step-definitions/smartBear-steps/smartBearSteps.js
@@ -0,0 +1,198 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+
+Given(/^user is on "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should see "([^"]*)" message$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should be routed to "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should be routed to "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^validate below menu items are displayed$/, () => {
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should be routed to "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^all rows should be checked$/, () => {
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^all rows should be unchecked$/, () => {
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should be routed to "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^validate all orders are deleted from the List of All Orders$/, () => {
+  return true
+})
+
+Then(/^validate user sees "([^"]*)" message$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters username as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters password as "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^user should be routed to "([^"]*)"$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" menu item$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user enters all product information$/, () => {
+  return true
+})
+
+When(/^user enters all address information$/, () => {
+  return true
+})
+
+When(/^user enters all payment information$/, () => {
+  return true
+})
+
+When(/^user clicks on "([^"]*)" button$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+When(/^user clicks on "([^"]*)" menu item$/, (args1) => {
+  console.log(args1)
+  return true
+})
+
+Then(/^validate all information entered displayed correct with the order$/, () => {
+  return true
+})
